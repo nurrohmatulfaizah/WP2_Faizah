0236fe38916ff7759c00b8927b7704d3013a8c98
nurrohmatulfaizah <nurrohmatulfaizah45@gmail.com>
2021-09-21 23:39:11 +0700

    Menambahkan Latihan.php

---
+++ Latihan.php
@@ -0,0 +1,11 @@
+<?php
+    class Latihan extends CI_Controller
+    {
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo "Nama saya Nur Rohmatul Faizah
+                    Saya tinggal di daerah Citra Raya, 
+                    Hobi saya adalah nonton film";   
+        }
+    }
